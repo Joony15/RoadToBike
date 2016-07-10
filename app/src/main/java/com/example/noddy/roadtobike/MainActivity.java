68e93d2907d0e43375ef0ef5d4c33e90df4da5a8
Administrator <noddyk15@gmail.com>
2016-07-10 22:38:07 +0900

    메인페이지 ui 임시완성

--- app/src/main/java/com/example/noddy/roadtobike/MainActivity.java
+++ app/src/main/java/com/example/noddy/roadtobike/MainActivity.java
@@ -6,18 +6,20 @@ package com.example.noddy.roadtobike;
 *
 */
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button MountGo;
     Button SeaGo;
-    Button WhereGo;
+    //Button WhereGo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +27,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         setContentView(R.layout.activity_main);
 
+        // roadtobike 이미지 띄우기 코드
+        Drawable drawable = getResources().getDrawable(
+                R.drawable.roadtobike);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView01);
+        imageView.setImageDrawable(drawable);
+
+
         /*Button for main menu*/
         MountGo = (Button) findViewById(R.id.mountchoice_btn);
         SeaGo = (Button) findViewById(R.id.seachoice_btn);
-        WhereGo = (Button) findViewById(R.id.wherechoice_btn);
+        //WhereGo = (Button) findViewById(R.id.wherechoice_btn);
         MountGo.setOnClickListener(this);
         SeaGo.setOnClickListener(this);
-        WhereGo.setOnClickListener(this);
+        //WhereGo.setOnClickListener(this);
 
 
 
@@ -54,11 +63,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         {
           intent = new Intent(this, SeaRoad.class);
           startActivity(intent);
-        }else if(v.getId() == R.id.wherechoice_btn)
-        {
-          intent = new Intent(this, WhereBike.class);
-          startActivity(intent);
-        }
+        }//else if(v.getId() == R.id.wherechoice_btn)
+        //{
+          //intent = new Intent(this, WhereBike.class);
+          //startActivity(intent);
+        //}
 
 
     }
